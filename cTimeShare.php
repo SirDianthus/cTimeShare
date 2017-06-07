0386ed99874e8d89d7dc5779dba08265fe3c36c2
SirDianthus <wkkinzie@gmail.com>
2017-06-07 03:18:55 -0500

    changed name and added wp comment headers

---
+++ cTimeShare.php
@@ -0,0 +1,10 @@
+<?php
+/**
+  * Plugin Name: Google Time Sharing
+  * Plugin URI: http://local.git
+  * Description: This plugin should add several pages to view schedule data stored in a database and block off your own and request others.
+  * Version: 0.0.1
+  * Author: Sir Dianthus
+  * Author URI: http://theripper.info
+  * License: GPL2
+  */
